6a754279ddeeed93338f338b3dcf576ffa3f491f
Lukasz <lukasz.maniak22@gmail.com>
2023-11-20 22:30:15 +0100

    main

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">yup</div>;
+  return (
+    <div className="app">
+      <ScoreHeader />
+    </div>
+  );
 }
+
+function ScoreHeader() {
+  return (
+    <div className="score-header">
+      <div className="header-left">
+        <p>ROCK</p>
+        <p>PAPER</p>
+        <p>SCISSORS</p>
+      </div>
+
+      <div className="header-right">
+        <p>SCORE</p>
+        <p>12</p>
+      </div>
+    </div>
+  );
+}
+
+function GameBoard() {
+  return <div></div>;
+}
+
+function GameStep1() {}
+function GameStep2() {}
+function GameStep3() {}
+function GameStep4() {}
 
 export default App;
